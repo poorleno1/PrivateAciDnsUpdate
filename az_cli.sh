bc1c100736a9d2d394ea857e4d803dc487ff4726
Jarek Olechno <jarekole@gmail.com>
2023-09-05 08:35:40 +0200

    Update az_cli.sh

--- az_cli.sh
+++ az_cli.sh
@@ -8,6 +8,10 @@ ACI_IP=$(az container show --name $ACI_INSTANCE_NAME --resource-group $RESOURCE_
 
 echo $ACI_IP
 
+current_dns_ip=$(az network private-dns record-set a show --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --query aRecords[0].ipv4Address)
+
+echo $current_dns_ip
+
 az network private-dns record-set a update --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP
 
 echo "Done"
